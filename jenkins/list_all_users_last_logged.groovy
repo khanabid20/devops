49321bcec5d4c935a50dc15d423086130741ab36
Abid Khan <33711981+khanabid20@users.noreply.github.com>
2022-09-21 10:32:07 +0530

    Create list_all_users_last_logged.groovy

---
+++ jenkins/list_all_users_last_logged.groovy
@@ -0,0 +1,25 @@
+import org.acegisecurity.*
+import hudson.model.User
+import hudson.tasks.Mailer
+import jenkins.security.*
+import java.util.Date
+
+// todo - yet to write code for checking last logged in users
+
+User.getAll().each{ u ->
+  def prop = u.getProperty(LastGrantedAuthoritiesProperty)
+  def mailAddress = u.getProperty(Mailer.UserProperty.class).getAddress()
+  def realUser = false
+  def timestamp = null
+  if (prop) {
+    realUser=true
+    timestamp = new Date(prop.timestamp).toString()
+  }
+
+  if (realUser){
+    println u.getId() + ':' + mailAddress + ':' + u.getDisplayName() + ':Jenkins-User:' + u?.getDescription() + ':timestamp=' + timestamp
+  } else if (realUser==false){
+    println u.getId() + ':' + u.getDisplayName() + ':No-Jenkins-User:' + u?.getDescription()
+  }
+}
+return
